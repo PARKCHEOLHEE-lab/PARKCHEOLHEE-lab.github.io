75ff596d47e8127e73021eaaf3d19cd96c75c35d
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2023-04-05 21:29:03 +0900

    ifc wall

--- threesrc/wall.js
+++ threesrc/wall.js
@@ -3,87 +3,106 @@ import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitCo
 import { IFCLoader } from "web-ifc-three";
 import { IFCSPACE } from "web-ifc";
 
-// import { IFCLoader } from "web-ifc-three";
+class IfcWall {
+    constructor() {
+        this.container = document.getElementById("threejs");
 
-// class DomIno {
-//   constructor () {
-//     this.container = document.getElementById("threejs");
+        this._setScene();
+        this._setAxes();
+        this._setGrid();
+        this._setLights();
+        this._setCamera();
+        this._setRenderer();
+        this._setOrbit();
+        this._setGeometry();
+    }
 
-//     this._setScene();
-//     this._setCamera();
-//     this._setRenderer();
-//     this._setOrbit();
-//     this._setGeometry();
-//   }
+    _setScene() {
+        this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color(0xffffff);
+    }
 
-//   _setScene () {
-//     this.scene = new THREE.Scene();
-//     this.scene.background = new THREE.Color(0xffffff);
-//   }
+    _setAxes() {
+        const axes = new THREE.AxesHelper();
+        axes.material.depthTest = false;
+        axes.renderOrder = 1;
+        this.scene.add(axes);
+    }
 
-//   _setCamera () {
-//     this.camera = new THREE.PerspectiveCamera(45, this.container.clientWidth / (this.container.clientWidth / 2), 0.1, 1000); 
-//     this.camera.position.x = 10;
-//     this.camera.position.y = 1;
-//     this.camera.position.z = 7;
-//   }
+    _setLights() {
+        const lightColor = 0xffffff;
 
-//   _setRenderer () {
-//     this.renderer = new THREE.WebGLRenderer();
-//     this.container.appendChild(this.renderer.domElement);
-//   }
+        const ambientLight = new THREE.AmbientLight(lightColor, 0.5);
+        ambientLight.castShadow = true;
+        this.scene.add(ambientLight);
 
-//   _setOrbit () {
-//     this.orbit = new OrbitControls(this.camera, this.renderer.domElement);
-//   }
+        const directionalLight = new THREE.DirectionalLight(lightColor, 1);
+        directionalLight.position.set(100, 100, 0);
+        directionalLight.target.position.set(0, 0, 0);
+        this.scene.add(directionalLight);
+        this.scene.add(directionalLight.target);
+        directionalLight.castShadow = true;
+    }
 
-//   _setGeometry () {
-//     let loader = new OBJLoader();
+    _setCamera() {
+        this.camera = new THREE.PerspectiveCamera(5, this.container.clientWidth / (this.container.clientWidth / 2), 0.1, 1000);
+        this.camera.position.set(60, 60, 60);
+    }
 
-//     loader.load(
-//       "../model/dom-ino/dom-ino.obj",
-//       function ( object ) {
-//         object.traverse(function ( child ) {
-//           if (child instanceof THREE.Mesh) {
-//               child.material = new THREE.MeshBasicMaterial({color: 0x808080});
-//             }
-//           }
-//         )
-//         scene.add( object );
-//       },
-//       function () {
-//       },
-//       function ( onError ) {
-//         console.log(onError);
-//       }
-//     )
+    _setGrid() {
+        const gridHelper = new THREE.GridHelper(3, 10);
+        this.scene.add(gridHelper);
+    }
 
-//     let container = this.container;
-//     let renderer = this.renderer
-//     let scene = this.scene
-//     let camera = this.camera
-//     let gridHelper = new THREE.GridHelper(3, 10);
-    
-//     scene.add(gridHelper);
-//     animate();
+    _setRenderer() {
+        this.renderer = new THREE.WebGLRenderer();
+        this.container.appendChild(this.renderer.domElement);
+    }
 
-//     this.orbit.update();
+    _setOrbit() {
+        this.orbit = new OrbitControls(this.camera, this.renderer.domElement);
+    }
 
-//     function animate () {
-//       requestAnimationFrame(animate);
+    _setGeometry() {
+        const container = this.container;
+        const renderer = this.renderer
+        const scene = this.scene
+        const camera = this.camera
 
-//       renderer.render(scene, camera);
+        const planeGeometry = new THREE.PlaneGeometry(100, 100);
+        const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+        const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+        plane.rotation.x = Math.PI * -0.5;
+        plane.receiveShadow = true;
+        scene.add(plane);
 
-//       resize();
-//     }
+        const ifcLoader = new IFCLoader();
+        ifcLoader.ifcManager.setWasmPath('https://unpkg.com/web-ifc@0.0.36/', true);
 
-//     function resize () {
-//       camera.aspect = container.clientWidth / (container.clientWidth / 2);
-//       camera.updateProjectionMatrix();
-//       renderer.setSize(container.clientWidth, container.clientWidth / 2);
-//     }
-//   }
-  
-// }
+        ifcLoader.load('../realbuilding/wall.ifc', function (model) {
+            scene.add(model.mesh);
 
-// new DomIno();
+        });
+
+        animate();
+
+        this.orbit.update();
+
+        function animate() {
+            requestAnimationFrame(animate);
+
+            renderer.render(scene, camera);
+
+            resize();
+        }
+
+        function resize() {
+            camera.aspect = container.clientWidth / (container.clientWidth / 2);
+            camera.updateProjectionMatrix();
+            renderer.setSize(container.clientWidth, container.clientWidth / 2);
+        }
+    }
+
+}
+
+new IfcWall();
